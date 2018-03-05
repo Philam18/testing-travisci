cc2c80760f88dbc75707f45ae28fb542bd27fd73
Philam18 <philiplam818@gmail.com>
2018-03-05 09:09:20 -0500

    n2

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer((req,res)=>{
   console.log(req);
   res.writeHead(200, {'Content-Type' : 'text/plain'});
-  res.end('Hello Travis!\n');
+  res.end('Hello Travis!\n')
 }).listen(1880,'127.0.0.1');
 console.log("Server running");
