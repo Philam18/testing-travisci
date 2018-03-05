ecc3a8802056db23df31d87aef3d112939f1997c
Philam18 <philiplam818@gmail.com>
2018-03-05 10:10:51 -0500

    redo

--- app.js
+++
@@ -1,9 +0,0 @@
-/* jshint esversion: 6 */
-var http = require('http');
-http.createServer((req,res)=>{
-  console.log(req);
-  res.writeHead(200,{'Content-type' : 'text/plain'});
-  res.end('Hello world!');
-}).listen(8000, '127.0.0.1');
-
-console.log("server on!");
