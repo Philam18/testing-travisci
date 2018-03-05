95c6bad9ba02b169e16b97afc726732befc568b4
Philam18 <philiplam818@gmail.com>
2018-03-05 09:07:33 -0500

    initcommit

---
+++ hello.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer((req,res)=>{
+  console.log(req);
+  res.writeHead(200, {'Content-Type' : 'text/plain'});
+  res.end('Hello Travis!\n');
+}).listen(1880,'127.0.0.1');
+console.log("Server running");
